44eeb71be21bebe280016605dcf2da0bccdd4035
hardik <dev.hardik.parashar@gmail.com>
2017-10-12 18:40:58 +0530

    dirfix

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+var Viewer = window.Viewer;
+var options = {
+	navbar:false,
+	title:false,
+	toolbar:false,
+	movable:false,
+	tooltip:false,
+	movable:false,
+	rotatable:false,
+	//transition:false,
+	keyboard:false,
+	
+	viewed:function(e)
+	{
+		console.log(e.type);
+	}
+};
+var viewer = new Viewer(document.getElementById('image'), options);
+};
